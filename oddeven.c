56f9dd4a4d9743f8368e30aae67a640e6c93cb75
Manvendra Pratap Singh <68546415+manvendra165@users.noreply.github.com>
2020-12-21 22:02:16 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int num;
+    printf("enter the no\n");
+    scanf("%d",&num);
+    if(num%2==0)
+    printf("no is even");
+    else
+    printf("no is odd");
+    
+    return 0;
+}
+
