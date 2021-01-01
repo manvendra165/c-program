63e6f35dbb952c621308370b97bd6d70bd7dc355
Manvendra Pratap Singh <68546415+manvendra165@users.noreply.github.com>
2021-01-01 20:33:33 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<stdio.h> //header file include:preprocessor directive
+int main()
+{
+    /* code */
+    //input -> process(logic) -> output // principle of computer
+    int num;
+    printf("hello i am using c commands ");
+   
+    scanf("%d ",&num);
+    printf("your  no=%d ",num);
+    return 0;
+
+    
+}
+//function is a block of code which have name and can be execute again and again ,
+// it may be possible to pass arguments to function and it can return value if required
