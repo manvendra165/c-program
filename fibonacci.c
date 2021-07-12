f90b5730877e076ea3ae08996e34e3c5163f5b97
Manvendra Pratap Singh <68546415+manvendra165@users.noreply.github.com>
2021-07-12 22:14:50 +0530

    Add files via upload

--- fibonacci.c
+++ fibonacci.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
     int a=0,b=1,term,num,i;
     printf("enter the no of terms up to which u want fibonacci series :");
     scanf("%d",&term);
-    for(i=1;i<=term;i++)
+    for(i=1;i<=100;i++)
     {
         printf(" %d",a);
         num=a+b;
